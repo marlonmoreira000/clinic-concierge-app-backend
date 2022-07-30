32e3889477b121f08e741c82447a6f59a8a1f12e
RichaPujara <91420098+RichaPujara@users.noreply.github.com>
2022-07-30 11:51:18 +1000

    added rolecheck to user routes

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -3,12 +3,14 @@ const router = express.Router();
 const UserModel = require("../models/userModel.js");
 const auth = require("../middleware/auth.js");
 const { findAll, findById } = require("../utils/dbUtils");
+const { roleCheck } = require("../middleware/roleCheck.js");
 
-router.get("/", auth, (req, res) => {
+
+router.get("/", auth,roleCheck("doctor"), (req, res) => {
   findAll(UserModel, {}, res);
 });
 
-router.get("/:id", auth, (req, res) => {
+router.get("/:id", auth,roleCheck("doctor"), (req, res) => {
   findById(UserModel, req.params.id, res);
 });
 
